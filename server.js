829b0dcce9d2068f6909222376dbc17c67cb5bab
Andy Rubiano <ar.andy.rubiano@gmail.com>
2024-09-09 20:15:28 -0500

    feat: 404 don't found request error

--- server.js
+++ server.js
@@ -12,6 +12,11 @@ app.disable('x-powered-by');
 
 app.use('/api', sendEmailRequests);
 
+// Para todas las peticiones que no encuentra, se le coloca el 404, es importante dejar al final de todas las peticiones
+app.use((request, response) => {
+    request.status(404).send('<h1>Error 404</h1>')
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
